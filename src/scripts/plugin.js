00a67f512cae4a1297c77a83c22ee77412679c48
arfullight <tmimvhuang@gmail.com>
2019-12-21 21:27:44 -0700

    remove unused function

--- src/scripts/plugin.js
+++ src/scripts/plugin.js
@@ -1,21 +1,5 @@
 import activateThebelab from './activateThebelab';
 
-const insertPreWithBinder = (editor) => {
-  // get selected element
-  const selection = editor.getSelection();
-  const range = selection.getRanges()[0];
-  const clone = range.cloneContents();
-
-  // wrap selected text within a pre tag
-  // todo: need to consider a case when having multiple
-  //       nodes in `clone`
-  const element = editor.document.createElement('pre');
-  element.setAttribute('data-executable', 'true');
-  element.setAttribute('class', 'code');
-  element.append(clone);
-  editor.insertElement(element);
-};
-
 const thebelabConfig = {
   binderOptions: {
     repo: 'binder-examples/requirements',
