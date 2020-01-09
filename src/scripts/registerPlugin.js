5ae509c5bd020b0e80316ebc31685e8e2856bb01
Miniland1333 <Miniland1333@users.noreply.github.com>
2020-01-08 17:07:47 -0800

    Move toolbar button initialization into registerPlugin.js (#19)
    This migrates the creation of the toolbar button to within the plugin.
    
    If desired, we can move the CKEDITOR.config.extraPlugins initialization to the LibreEditor code.
    
    Important TODO: Remove the duplicate code from the editor configuration once this is merged into master.

--- src/scripts/registerPlugin.js
+++ src/scripts/registerPlugin.js
@@ -2,7 +2,10 @@ import loadPlugin from './plugin';
 import activateThebelab from './activateThebelab';
 
 // Adds this plugin to the LibreEditor for later activation
-LibreEditor.binderPlugin = loadPlugin;
+LibreEditor.binderPlugin = (config) => {
+  loadPlugin();
+  config.toolbar[12].push('enableBinder');
+};
 
 // activate thebelab on every page if data-executable exists
 $(document).ready(() => { activateThebelab(); });
