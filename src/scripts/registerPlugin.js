408655390808f78a8b892df7a002c331f403cd8b
arfullight <tmimvhuang@gmail.com>
2020-03-10 22:13:46 -0700

    bind the setter to detech when datatable plugin is loaded

--- src/scripts/registerPlugin.js
+++ src/scripts/registerPlugin.js
@@ -8,12 +8,22 @@ LibreEditor.binderPlugin = (config) => {
   config.toolbar[12].push('enableBinder');
 };
 
-while (true) {
-  if ($ !== undefined && $.fn.dataTable !== undefined) {
-    loadScript('https://cdnjs.cloudflare.com/ajax/libs/require.js/2.3.6/require.min.js');
-    break;
-  }
-}
+// overwrite getter and setter of $.fn.dataTable because we need to load requirejs after it is set
+Object.defineProperty($.fn, 'dataTable', {
+  get() {
+    return this.privateDataTable;
+  },
+  set(value) {
+    // need this to avoid infinite loops
+    // if we just use `this.dataTable`, it will trigger the setter again
+    this.privateDataTable = value;
+
+    // check if requirejs is already there
+    if (typeof define !== 'function' || !define.amd) {
+      loadScript('https://cdnjs.cloudflare.com/ajax/libs/require.js/2.3.6/require.min.js');
+    }
+  },
+});
 
 // activate thebelab on every page if data-executable exists
 $(document).ready(() => { activateThebelab(); });
