422767177b0b0505e700691cc38472d2468cd5a6
arfullight <tmimvhuang@gmail.com>
2020-03-24 16:28:53 -0700

    update deploy script to use staging

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -2,7 +2,8 @@ HOME_DIR=/home/jupyterteam
 
 ssh -tt jupyterteam@test.libretexts.org << EOF
 cd $HOME_DIR/ckeditor-binder-plugin
-git fetch --all && git reset --hard origin/master
+git fetch --all
+git checkout staging && git reset --hard origin/staging
 yarn install
 yarn build
 cp build/js/registerPlugin.min.js* $HOME_DIR/public
