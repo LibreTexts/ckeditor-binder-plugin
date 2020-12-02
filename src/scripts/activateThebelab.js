bd5a8070faf6860d73bffdd7df2d4957e87f06cd
sandertyu <noasanders@ucdavis.edu>
2020-12-01 17:47:55 -0800

    bump to default-env 2.2

--- src/scripts/activateThebelab.js
+++ src/scripts/activateThebelab.js
@@ -25,7 +25,7 @@ const getConfig = (language) => {
   const config = {
     binderOptions: {
       repo: 'LibreTexts/default-env',
-      ref: '2.1.1',
+      ref: '2.2',
       binderUrl,
     },
     kernelOptions: {
